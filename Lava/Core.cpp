9b8315ff78d3fe6f9a01f5092cbc0cf43c2b4ff5
PureFoxCore <icefoxtx@gmail.com>
2021-08-11 08:20:43 +0400

    spacing correction

--- Lava/Core.cpp
+++ Lava/Core.cpp
@@ -12,12 +12,11 @@ int main()
     Lava::Logger::Init();
 
     int glfwstatus = glfwInit();
-
     LAVA_ASSERT(glfwstatus, "can't initialize glfw");
 
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow *window = glfwCreateWindow(1280, 720, "LAVA - Vulkan window", nullptr, nullptr);
 
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "LAVA - Vulkan window", nullptr, nullptr);
     LAVA_ASSERT(window, "can't create window");
 
     uint32_t extensionCount = 0;
