9e40e58dec0f862489e4a5ee2122c06c8c6982f5
PureFoxCore <icefoxtx@gmail.com>
2021-08-11 08:20:17 +0400

    Fixed init and added asserts

--- Lava/Core.cpp
+++ Lava/Core.cpp
@@ -11,13 +11,19 @@ int main()
 {
     Lava::Logger::Init();
 
+    int glfwstatus = glfwInit();
+
+    LAVA_ASSERT(glfwstatus, "can't initialize glfw");
+
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
     GLFWwindow *window = glfwCreateWindow(1280, 720, "LAVA - Vulkan window", nullptr, nullptr);
+
+    LAVA_ASSERT(window, "can't create window");
 
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
 
-    LAVA_INFO("{0} extensions supported", extensionCount);
+    LAVA_INFO("{0} extensions supported:", extensionCount);
 
     glm::mat4 matrix;
     glm::vec4 vec;
