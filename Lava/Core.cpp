23c79fa15d79b3220ca65cba8241474b09eb53d9
PureFoxCore <icefoxtx@gmail.com>
2021-08-11 07:45:38 +0400

    Test code

--- Lava/Core.cpp
+++ Lava/Core.cpp
@@ -1,4 +1,34 @@
+#include <GLFW/glfw3.h>
+
+#define GLM_FORCE_RADIANS
+#define GLM_FORCE_DEPTH_ZERO_TO_ONE
+#include <glm/vec4.hpp>
+#include <glm/mat4x4.hpp>
+
+#include "Logger.hpp"
+
 int main()
 {
-    return 0;
+    Lava::Logger::Init();
+
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+    GLFWwindow *window = glfwCreateWindow(1280, 720, "LAVA - Vulkan window", nullptr, nullptr);
+
+    uint32_t extensionCount = 0;
+    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+    LAVA_INFO("{0} extensions supported", extensionCount);
+
+    glm::mat4 matrix;
+    glm::vec4 vec;
+    auto test = matrix * vec;
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+    return EXIT_SUCCESS;
 }
